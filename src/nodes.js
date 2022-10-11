31019336bd8c737e9e5599cae0fe4cd8e63e6c72
Nathan Laha <nlaha@outlook.com>
2022-10-11 15:46:46 -0700

    finish gameplay events in preparation for transition to react-flow

--- src/nodes.js
+++ src/nodes.js
@@ -1,105 +1,126 @@
 import { FlumeConfig, Colors, Controls } from "flume";
 
 export default function create_nodes(config) {
-  return (
-    config
-      .addRootNodeType({
-        type: "dialogue_entry",
-        label: "Dialogue Entry",
-        initialWidth: 170,
-        inputs: (ports) => [
-          ports.string({
-            name: "npc_name",
-            label: "NPC Name",
-          }),
-        ],
-        outputs: (ports) => [
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-        ],
-      })
-      // gameplay event node
-      .addNodeType({
-        type: "gameplay_event",
-        label: "Gameplay Event",
-        initialWidth: 170,
-        inputs: (ports) => [
-          ports.gameplay_event({
-            name: "gameplay_event",
-            label: "Gameplay Event",
-          }),
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-        ],
-        outputs: (ports) => [
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-        ],
-      })
-      .addNodeType({
-        type: "metadata",
-        label: "Metadata",
-        description:
-          "Tags a flow with metadata. This can be used in an engine for a variety of things.",
-        inputs: (ports) => [
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-          ports.string({
-            name: "key",
-            label: "Key",
-          }),
-          ports.string({
-            name: "value",
-            label: "Value",
-          }),
-        ],
-        outputs: (ports) => [
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-        ],
-      })
-      .addNodeType({
-        type: "deep_learning",
-        label: "Deep Learning",
-        description:
-          "Initializes a conversation with the BERT deep learning model.",
-        inputs: (ports) => [
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-          ports.string({
-            name: "dl_initial_prompt",
-            label: "Initial Prompt",
-          }),
-          ports.number({
-            name: "num_dl_conversation_steps",
-            label: "Number of AI conversation steps",
-          }),
-        ],
-        outputs: (ports) => [
-          ports.flow({
-            name: "flow",
-            label: "Flow",
-          }),
-        ],
-      })
-      .addNodeType({
-        type: "dialogue_event_yes_no",
-        label: "Dialogue Event (Yes/No)",
-        description: "Represents a dialogue event with a yes/no response.",
-        initialWidth: 160,
-        inputs: (ports) => [
+  return config
+    .addRootNodeType({
+      type: "dialogue_entry",
+      label: "Dialogue Entry",
+      initialWidth: 170,
+      inputs: (ports) => [
+        ports.string({
+          name: "npc_name",
+          label: "NPC Name",
+        }),
+      ],
+      outputs: (ports) => [
+        ports.flow({
+          name: "flow",
+          label: "Flow",
+        }),
+      ],
+    })
+    .addNodeType({
+      type: "metadata",
+      label: "Metadata",
+      description:
+        "Tags a flow with metadata. This can be used in an engine for a variety of things.",
+      inputs: (ports) => [
+        ports.flow({
+          name: "flow",
+          label: "Flow",
+        }),
+        ports.string({
+          name: "key",
+          label: "Key",
+        }),
+        ports.string({
+          name: "value",
+          label: "Value",
+        }),
+      ],
+      outputs: (ports) => [
+        ports.flow({
+          name: "flow",
+          label: "Flow",
+        }),
+      ],
+    })
+    .addNodeType({
+      type: "deep_learning",
+      label: "Deep Learning",
+      description:
+        "Initializes a conversation with the BERT deep learning model.",
+      inputs: (ports) => [
+        ports.flow({
+          name: "flow",
+          label: "Flow",
+        }),
+        ports.string({
+          name: "dl_initial_prompt",
+          label: "Initial Prompt",
+        }),
+        ports.number({
+          name: "num_dl_conversation_steps",
+          label: "Number of AI conversation steps",
+        }),
+      ],
+      outputs: (ports) => [
+        ports.flow({
+          name: "flow",
+          label: "Flow",
+        }),
+      ],
+    })
+    .addNodeType({
+      type: "dialogue_event_yes_no",
+      label: "Dialogue Event (Yes/No)",
+      description: "Represents a dialogue event with a yes/no response.",
+      initialWidth: 160,
+      inputs: (ports) => [
+        ports.flow({
+          name: "flow",
+          label: "Flow",
+        }),
+        ports.string({
+          name: "npc_text",
+          label: "NPC Text",
+        }),
+      ],
+      outputs: (ports) => [
+        ports.flow({
+          name: "player_pick_0",
+          label: "No",
+        }),
+        ports.flow({
+          name: "player_pick_1",
+          label: "Yes",
+        }),
+      ],
+    })
+    .addNodeType({
+      type: "dialogue_event",
+      label: "Dialogue Event",
+      description:
+        "Represents a dialogue event with a number of options and NPC text",
+      initialWidth: 160,
+      inputs: (ports) => (data) => {
+        let player_response_inputs = [];
+
+        if (data.num_player_responses) {
+          let num_player_responses = data.num_player_responses.number;
+
+          // make player respose inputs using the number of player responses
+          for (let i = 0; i < num_player_responses; i++) {
+            player_response_inputs.push(
+              ports.string({
+                name: `player_response_${i}`,
+                label: `Player Response ${i}`,
+              })
+            );
+          }
+        }
+
+        return [
           ports.flow({
             name: "flow",
             label: "Flow",
@@ -108,86 +129,38 @@ export default function create_nodes(config) {
             name: "npc_text",
             label: "NPC Text",
           }),
-        ],
-        outputs: (ports) => [
-          ports.flow({
-            name: "player_pick_0",
-            label: "No",
+          ports.number({
+            name: "num_player_responses",
+            label: "Number of Player Responses",
           }),
+          ...player_response_inputs,
+        ];
+      },
+      outputs: (ports) => (data, connections) => {
+        let player_response_outputs = [
           ports.flow({
-            name: "player_pick_1",
-            label: "Yes",
+            name: `player_pick_none`,
+            label: `Flow`,
           }),
-        ],
-      })
-      .addNodeType({
-        type: "dialogue_event",
-        label: "Dialogue Event",
-        description:
-          "Represents a dialogue event with a number of options and NPC text",
-        initialWidth: 160,
-        inputs: (ports) => (data) => {
-          let player_response_inputs = [];
+        ];
 
-          if (data.num_player_responses) {
-            let num_player_responses = data.num_player_responses.number;
+        if (data.num_player_responses && data.num_player_responses.number > 0) {
+          let num_player_responses = data.num_player_responses.number;
 
-            // make player respose inputs using the number of player responses
-            for (let i = 0; i < num_player_responses; i++) {
-              player_response_inputs.push(
-                ports.string({
-                  name: `player_response_${i}`,
-                  label: `Player Response ${i}`,
-                })
-              );
-            }
+          player_response_outputs = [];
+          // make player response outputs using the number of player responses
+          for (let i = 0; i < num_player_responses; i++) {
+            player_response_outputs.push(
+              ports.flow({
+                name: `player_pick_${i}`,
+                label: `"${
+                  (data[`player_response_${i}`] || { string: "" }).string
+                }"`,
+              })
+            );
           }
-
-          return [
-            ports.flow({
-              name: "flow",
-              label: "Flow",
-            }),
-            ports.string({
-              name: "npc_text",
-              label: "NPC Text",
-            }),
-            ports.number({
-              name: "num_player_responses",
-              label: "Number of Player Responses",
-            }),
-            ...player_response_inputs,
-          ];
-        },
-        outputs: (ports) => (data, connections) => {
-          let player_response_outputs = [
-            ports.flow({
-              name: `player_pick_none`,
-              label: `Flow`,
-            }),
-          ];
-
-          if (
-            data.num_player_responses &&
-            data.num_player_responses.number > 0
-          ) {
-            let num_player_responses = data.num_player_responses.number;
-
-            player_response_outputs = [];
-            // make player response outputs using the number of player responses
-            for (let i = 0; i < num_player_responses; i++) {
-              player_response_outputs.push(
-                ports.flow({
-                  name: `player_pick_${i}`,
-                  label: `"${
-                    (data[`player_response_${i}`] || { string: "" }).string
-                  }"`,
-                })
-              );
-            }
-          }
-          return [...player_response_outputs];
-        },
-      })
-  );
+        }
+        return [...player_response_outputs];
+      },
+    });
 }
